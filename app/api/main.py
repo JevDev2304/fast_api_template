648d802c74d8c398d05951ff10622cd2984cdcf6
JevDev2304 <jvaldes245@soyudemedellin.edu.co>
2025-04-30 22:21:24 -0500

    I maked better presentation of my root path using HTML

--- app/api/main.py
+++ app/api/main.py
@@ -2,16 +2,17 @@
 from fastapi import FastAPI
 from app.api.routes.endpoint_01 import router as endpoint_01_router
 from app.api.routes.endpoint_02 import router as endpoint_02_router
+import os
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 app.include_router(endpoint_01_router)
 app.include_router(endpoint_02_router)
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def read_root():
-    return {"message": "Hello, FastAPI!"}
-
-@app.get("/health")
-def health_check():
-    return {"status": "ok"}
+    path = os.path.join(os.path.dirname(__file__), "../static/presentation.html")
+    with open(path, "r", encoding="utf-8") as f:
+        html_content = f.read()
+    return HTMLResponse(content=html_content)
